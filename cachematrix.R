dfdad6014a94528ef8e812922e13fef5353561c2
ddiakun <42541141+ddiakun@users.noreply.github.com>
2018-08-20 08:31:03 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+#  write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+# Write the following functions:
 
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+#   If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+#   retrieve the inverse from the cache.
+
+# Computing the inverse of a square matrix can be done with the solve function in R. For example, 
+#   if X is a square invertible matrix, then solve(X) returns its inverse.
+
+# For this assignment, assume that the matrix supplied is always invertible.
+
+
+# makeCacheMatrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL
+  set <- function(y) {
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inver <<- inverse
+  getinverse <- function() inver
+  list(set=set, get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inver <- x$getinverse()
+  if(!is.null(inver)) {
+    message("getting cached data.")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data)
+  x$setinverse(inver)
+  inver
 }
